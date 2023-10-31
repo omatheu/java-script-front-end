5610cdf5a1d81fb92b671a3d2f711df1b89c8120
Matheus Ribeiro dos Santos <matheus.ribeiro@sou.inteli.edu.br>
2023-10-31 10:27:54 -0300

    aprendendo js - front-end

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,3 +1,51 @@
-function a(){
-    alert('olá mundo');
+function TocaSomPom(){
+    document.querySelector('#som_tecla_pom').play();
 }
+
+function TocaSomClap() {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+function TocaSomTim() {
+    document.querySelector('#som_tecla_tim').play();
+}
+
+function TocaSomPuff() {
+    document.querySelector('#som_tecla_puff').play();
+}
+
+function TocaSomSplash() {
+    document.querySelector('#som_tecla_splash').play();
+}
+
+function TocaSomToim() {
+    document.querySelector('#som_tecla_toim').play();
+}
+
+function TocaSomPsh() {
+    document.querySelector('#som_tecla_psh').play();
+}
+
+function TocaSomTic() {
+    document.querySelector('#som_tecla_tic').play();
+}
+
+function TocaSomTom() {
+    document.querySelector('#som_tecla_tom').play()
+}
+
+
+/*
+const tecla_t = 84;
+const pom = document.querySelector('.tecla_pom');
+
+function som_a(){
+    pom.addEventListener('keydown', (event) => {
+        if (event.keyCode === tecla_t){
+            pom.play();
+        }
+    })
+}
+
+som_a();
+*/
