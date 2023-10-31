039bb18047d7520dc89f2a6ed2ce2d75efab40f3
Matheus Ribeiro dos Santos <matheus.ribeiro@sou.inteli.edu.br>
2023-10-31 11:05:10 -0300

    update cod

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -2,50 +2,17 @@ function TocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
 
-function TocaSomClap() {
-    document.querySelector('#som_tecla_clap').play();
-}
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-function TocaSomTim() {
-    document.querySelector('#som_tecla_tim').play();
-}
-
-function TocaSomPuff() {
-    document.querySelector('#som_tecla_puff').play();
-}
-
-function TocaSomSplash() {
-    document.querySelector('#som_tecla_splash').play();
-}
-
-function TocaSomToim() {
-    document.querySelector('#som_tecla_toim').play();
-}
-
-function TocaSomPsh() {
-    document.querySelector('#som_tecla_psh').play();
-}
-
-function TocaSomTic() {
-    document.querySelector('#som_tecla_tic').play();
-}
-
-function TocaSomTom() {
-    document.querySelector('#som_tecla_tom').play()
-}
+listaDeTeclas[0].onclick = TocaSomPom;
 
 
-/*
-const tecla_t = 84;
-const pom = document.querySelector('.tecla_pom');
 
-function som_a(){
-    pom.addEventListener('keydown', (event) => {
-        if (event.keyCode === tecla_t){
-            pom.play();
-        }
-    })
-}
+// function iteraList(list){
+//     console.log(list[0]);   
+//     for (i in list){
+//         list[i].onclick = TocaSomPom.play;
+//     }
+// }
 
-som_a();
-*/
+// iteraList(listaDeTeclas)
