48025e6746807b45c48945dfd027a9dda940742d
Matheus Ribeiro dos Santos <matheus.ribeiro@sou.inteli.edu.br>
2023-11-03 08:44:10 -0300

    add sons nas teclas

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,18 +1,20 @@
-function TocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function TocaSom(idElemento){
+    document.querySelector(idElemento).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = TocaSomPom;
+
+let i = 0
+while (i < listaDeTeclas.length) {
 
 
+    console.log(i);
 
-// function iteraList(list){
-//     console.log(list[0]);   
-//     for (i in list){
-//         list[i].onclick = TocaSomPom.play;
-//     }
-// }
-
-// iteraList(listaDeTeclas)
+    //Funções anônimas no JS só podem ser utilizadas nesse contexto
+    //Isto é, quando armazenamos dentro de uma variável
+    listaDeTeclas[i].onclick = function () {
+        TocaSom('#som_tecla_pom');
+    };
+    i++;
+}
